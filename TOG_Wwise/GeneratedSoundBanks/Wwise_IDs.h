aef2bcea4c9faab4baba0f9d7bcbc860de5d66f4
rowincolra <bernardo92@nate.com>
2026-05-28 00:41:25 +0900

    wwise update 0528

--- TOG_Wwise/GeneratedSoundBanks/Wwise_IDs.h
+++ TOG_Wwise/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,12 +14,23 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID PLAY_BGM_LOOP = 3569852853U;
+        static const AkUniqueID PLAY_TOG_UI_BATTLE_CARD_DISCARD = 3290775020U;
+        static const AkUniqueID PLAY_TOG_UI_BATTLE_CARD_DRAW = 621582148U;
+        static const AkUniqueID PLAY_TOG_UI_BATTLE_CARD_HOLD = 3941666393U;
+        static const AkUniqueID PLAY_TOG_UI_BATTLE_CARD_PLACE = 153578335U;
+        static const AkUniqueID PLAY_TOG_UI_BATTLE_DECK_ATTACKBUTTON_CLICK = 4098112184U;
+        static const AkUniqueID PLAY_TOG_UI_BATTLE_DECK_ATTACKBUTTON_DISAPPEAR = 4112871447U;
+        static const AkUniqueID PLAY_TOG_UI_BATTLE_DECK_ATTACKBUTTON_HOVER = 1155813750U;
+        static const AkUniqueID PLAY_TOG_UI_CLICK_BUTTON = 2614728027U;
+        static const AkUniqueID PLAY_TOG_UI_CLICK_DEFAULT = 203955354U;
+        static const AkUniqueID PLAY_TOG_UI_CLICK_HOVER = 3141384431U;
     } // namespace EVENTS
 
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
         static const AkUniqueID BGM = 412724365U;
+        static const AkUniqueID UI = 1551306167U;
     } // namespace BANKS
 
     namespace BUSSES
