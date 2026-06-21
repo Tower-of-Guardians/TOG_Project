9ff78d5b45db912814468a574eeb561b8627ccd8
Jongmin Kim <vwmartin@naver.com>
2026-06-21 22:04:19 +0900

    fix: 빌드에 어드레서블을 포함하도록 변경

--- TOG_Unity/Assets/Editor/WindowsBuild.cs
+++ TOG_Unity/Assets/Editor/WindowsBuild.cs
@@ -1,12 +1,17 @@
+#if UNITY_EDITOR
 using System;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEditor.Build.Reporting;
+using UnityEditor.AddressableAssets;
+using UnityEditor.AddressableAssets.Settings;
 
 public class WindowsBuild
 {
     public static void PerformBuild()
     {
+        BuildAddressables();
+
         string[] scenes = FindEnabledEditorScenes();
 
         BuildPlayerOptions options = new BuildPlayerOptions
@@ -27,6 +32,24 @@ public class WindowsBuild
         Console.WriteLine("Build succeeded");
     }
 
+    private static void BuildAddressables()
+    {
+        AddressableAssetSettings settings = AddressableAssetSettingsDefaultObject.Settings;
+
+        if (settings == null)
+        {
+            throw new Exception("AddressableAssetSettings not found.");
+        }
+
+        Console.WriteLine("Cleaning Addressables content...");
+        AddressableAssetSettings.CleanPlayerContent();
+
+        Console.WriteLine("Building Addressables content...");
+        AddressableAssetSettings.BuildPlayerContent();
+
+        Console.WriteLine("Addressables build succeeded");
+    }
+
     private static string[] FindEnabledEditorScenes()
     {
         List<string> scenes = new List<string>();
@@ -42,3 +65,4 @@ public class WindowsBuild
         return scenes.ToArray();
     }
 }
+#endif
