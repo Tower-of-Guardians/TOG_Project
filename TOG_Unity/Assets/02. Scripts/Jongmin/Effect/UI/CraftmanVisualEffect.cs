3cbdbc83bd2095530d80a22ea421518ab76df320
Jongmin Kim <vwmartin@naver.com>
2026-08-18 21:44:08 +0900

    refactor: 대장장이 뷰와 연출 분리

---
+++ TOG_Unity/Assets/02. Scripts/Jongmin/Effect/UI/CraftmanVisualEffect.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using DG.Tweening;
+using JxModule;
+using UnityEngine;
+
+namespace Jongmin
+{
+    [ManagedEffect("UI", "Craftman", 100)]
+    public class CraftmanVisualEffect : MonoBehaviour
+    {
+        [BigHeader("Fade")]
+        [SerializeField] private float activeFade = 1f;
+        [SerializeField] private float deactiveFade = 0f;
+        [SerializeField] private float fadeDuration = 1f;
+        
+        private Tween _toggleTween;
+
+        public void PlayShowEffect(CanvasGroup canvasGroup, Action callback = null)
+        {
+            _toggleTween?.Kill();
+            _toggleTween = canvasGroup.DOFade(activeFade, fadeDuration).OnComplete(() =>
+            {
+                callback?.Invoke();
+                _toggleTween = null;
+            });
+        }
+
+        public void PlayHideEffect(CanvasGroup canvasGroup, Action callback = null)
+        {
+            _toggleTween?.Kill();
+            _toggleTween = canvasGroup.DOFade(deactiveFade, fadeDuration).OnComplete(() =>
+            {
+                callback?.Invoke();
+                _toggleTween = null;
+            });
+        }
+    }
+}
