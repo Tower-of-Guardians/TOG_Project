103963e5481d1bc238ca257fecc3be77aef7bfd5
Jongmin Kim <vwmartin@naver.com>
2026-09-09 01:02:51 +0900

    feat: 유물 디버그 커맨드 추가

---
+++ TOG_Unity/Assets/02. Scripts/Jongmin/Commands/TogTerminalCommandInstaller.cs
@@ -0,0 +1,16 @@
+﻿using JxModule.Terminal;
+using UnityEngine;
+
+namespace Jongmin
+{
+    public sealed class TogTerminalCommandInstaller : JxTerminalCommandInstallerBehaviour
+    {
+        [SerializeField] private RelicDomain relicDomain;
+        
+        public override void Install(JxTerminal terminal)
+        {
+            var relicCommands = new RelicCommands(relicDomain);
+            JxCommandAttributeRegistrar.RegisterCommands(terminal, relicCommands);
+        }
+    }
+}
