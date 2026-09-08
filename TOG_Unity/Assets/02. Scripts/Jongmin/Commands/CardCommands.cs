fd866eb8dcae07d898eb5ad23cd35fd2ef5f057f
Jongmin Kim <vwmartin@naver.com>
2026-09-09 01:37:58 +0900

    feat: 카드 디버그 커맨드 추가

---
+++ TOG_Unity/Assets/02. Scripts/Jongmin/Commands/CardCommands.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using JxModule.Terminal;
+
+namespace Jongmin
+{
+    public class CardCommands
+    {
+        private readonly HandDomain _handDomain;
+        private readonly FieldDomain _fieldDomain;
+
+        public CardCommands(HandDomain handDomain, FieldDomain fieldDomain)
+        {
+            _handDomain = handDomain;
+            _fieldDomain = fieldDomain;
+        }
+
+        [JxCommand("Card List")]
+        private IEnumerable<string> ListCards()
+        {
+            return DataCenter.card_datas.Keys.ToList();
+        }
+
+        [JxCommand("Add Hand")]
+        private void AddHand([JxOptionValue("Card List")] string cardId)
+        {
+            var battleCardData = new BattleCardData
+            {
+                index = 0,
+                data = DataCenter.card_datas[cardId]
+            };
+            
+            _handDomain.System.CreateCard(battleCardData);
+        }
+
+        [JxCommand("Remove Hand")]
+        private void RemoveHand([JxOptionValue("Card List")] string cardId)
+        {
+            var targetCard = _handDomain.Container.Cards.FirstOrDefault(card => card.CardData.id == cardId);
+            _handDomain.System.RemoveCard(targetCard);
+        }
+
+        [JxCommand("Add ATK Field")]
+        private void AddAtkField([JxOptionValue("Card List")] string cardId)
+        {
+            var battleCardData = new BattleCardData
+            {
+                index = 0,
+                data = DataCenter.card_datas[cardId]
+            };
+
+            _fieldDomain.AtkSystem.CreateCard(battleCardData);
+        }
+
+        [JxCommand("Remove ATK Field")]
+        private void RemoveAtkField([JxOptionValue("Card List")] string cardId)
+        {
+            var targetCard = _fieldDomain.AtkContainer.Cards.FirstOrDefault(card => card.CardData.id == cardId);
+            _fieldDomain.AtkSystem.RemoveCard(targetCard);
+        }
+
+        [JxCommand("Add DEF Field")]
+        private void AddDefField([JxOptionValue("Card List")] string cardId)
+        {
+            var battleCardData = new BattleCardData
+            {
+                index = 0,
+                data = DataCenter.card_datas[cardId]
+            };
+
+            _fieldDomain.DefSystem.CreateCard(battleCardData);
+        }
+
+        [JxCommand("Remove DEF Field")]
+        private void RemoveDefField([JxOptionValue("Card List")] string cardId)
+        {
+            var targetCard = _fieldDomain.DefContainer.Cards.FirstOrDefault(card => card.CardData.id == cardId);
+            _fieldDomain.DefSystem.RemoveCard(targetCard);
+        }
+    }
+}
--- TOG_Unity/Assets/02. Scripts/Jongmin/Commands/TogTerminalCommandInstaller.cs
+++ TOG_Unity/Assets/02. Scripts/Jongmin/Commands/TogTerminalCommandInstaller.cs
@@ -6,11 +6,16 @@ namespace Jongmin
     public sealed class TogTerminalCommandInstaller : JxTerminalCommandInstallerBehaviour
     {
         [SerializeField] private RelicDomain relicDomain;
+        [SerializeField] private HandDomain handDomain;
+        [SerializeField] private FieldDomain fieldDomain;
         
         public override void Install(JxTerminal terminal)
         {
             var relicCommands = new RelicCommands(relicDomain);
             JxCommandAttributeRegistrar.RegisterCommands(terminal, relicCommands);
+            
+            var cardCommands = new CardCommands(handDomain, fieldDomain);
+            JxCommandAttributeRegistrar.RegisterCommands(terminal, cardCommands);
         }
     }
 }
